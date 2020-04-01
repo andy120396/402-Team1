55712fac6b893eebfe388b8de7163db3788a20f2
andy liu <andy120396@yahoo.com>
2020-03-31 20:29:39 -0400

    Added script

--- functional_test.py
+++ functional_test.py
@@ -1,8 +1,22 @@
-#Dan decides to create an online bank account on a bank’s website.
-#When he opens the site, he sees a sign-up button
-#He clicks sign up and is prompted to enter his first name, last name.
-#The website tells him that his account is approved and shows dan a screen with his username and bank account balance that starts at zero
-#On the site main page, there are two buttons “deposit” and “withdraw”
-#Dan now clicks on “deposit” and deposits $1000 and we see the balance reflect that change
-#Dan now clicks on “withdraw” and withdraw $500 and we see the balance show the withdrawal’s details.
+import time
+from django.test import LiveServerTestCase
+from selenium import webdriver
+import unittest
+
+class NewUserTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def sample_test(self):
+        #Dan decides to create an online bank account on a bank’s website.
+        #When he opens the site, he sees a sign-up button
+        #He clicks sign up and is prompted to enter his first name, last name.
+        #The website tells him that his account is approved and shows dan a screen with his username and bank account balance that starts at zero
+        #On the site main page, there are two buttons “deposit” and “withdraw”
+        #Dan now clicks on “deposit” and deposits $1000 and we see the balance reflect that change
+        #Dan now clicks on “withdraw” and withdraw $500 and we see the balance show the withdrawal’s details.
 

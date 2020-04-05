0c62eb076f08dbb76016c5e94b0d9ad353ac8957
andy liu <andy120396@yahoo.com>
2020-04-05 13:02:44 -0400

    fixed eos error

--- functional_test.py
+++ functional_test.py
@@ -13,10 +13,16 @@ class NewUserTest(LiveServerTestCase):
 
     def sample_test(self):
         #Dan decides to create an online bank account on a bank’s website.
-        #When he opens the site, he sees a sign-up button
-        #He clicks sign up and is prompted to enter his first name, last name.
-        #The website tells him that his account is approved and shows dan a screen with his username and bank account balance that starts at zero
-        #On the site main page, there are two buttons “deposit” and “withdraw”
-        #Dan now clicks on “deposit” and deposits $1000 and we see the balance reflect that change
-        #Dan now clicks on “withdraw” and withdraw $500 and we see the balance show the withdrawal’s details.
+        self.assertIn('Bank Account', self.browser.title)
 
+        #When he opens the site, he sees a sign-up button
+
+        #He clicks sign up and is prompted to enter his first name, last name.
+
+        #The website tells him that his account is approved and shows dan a screen with his username and bank account balance that starts at zero
+
+        #On the site main page, there are two buttons “deposit” and “withdraw”
+
+        #Dan now clicks on “deposit” and deposits $1000 and we see the balance reflect that change
+
+        #Dan now clicks on “withdraw” and withdraw $500 and we see the balance show the withdrawal’s details.

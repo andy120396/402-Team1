41b9e5fdf73fd2beb0875b9bf355cab8e849a60b
andy liu <andy120396@yahoo.com>
2020-04-05 13:45:15 -0400

    started to code

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,7 @@
 import time
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 browser = webdriver.Firefox()
 
@@ -16,9 +17,21 @@ class NewUserTest(LiveServerTestCase):
         #Dan decides to create an online bank account on a bank’s website.
         self.browser.get('http://localhost:8000')
 
-        #When he opens the site, he sees a sign-up button
+        #When he opens the site, he sees a that the title has to do with Bank Accounts
+        self.assert('Bank Account', self.browser.title)
 
         #He clicks sign up and is prompted to enter his first name, last name.
+        inputbox = self.browser.find_element_by_id('id_new_user')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter your first and last name'
+
+        )
+
+        #Dan enters his name and presses Enter
+        inputbox.send_keys('Dan Miles')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         #The website tells him that his account is approved and shows dan a screen with his username and bank account balance that starts at zero
 

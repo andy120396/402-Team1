86deb5901f63a0548f5fb5c62baa5e4518e533b1
Ngoc7-psu <59721719+Ngoc7-psu@users.noreply.github.com>
2020-04-26 14:35:00 -0400

    html home update

--- lists/views.py
+++ lists/views.py
@@ -5,15 +5,7 @@ from lists.models import Item
 # Create your views here.
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
-    
     return render(request, 'home.html')
 
 def subpage(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
-    
     return render(request, 'subpage.html')
